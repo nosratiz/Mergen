fcc19519e9a860c92c42f440d0e1ad0a127f93b1
Ashkan Nourzadeh <ans.ashkan@gmail.com>
2019-06-14 19:15:15 +0430

    add AnswerChooseHistory fields to QuestionViewModel

--- MergenAPI/Mergen.Admin.Api/API/Questions/QuestionViewModel.cs
+++ MergenAPI/Mergen.Admin.Api/API/Questions/QuestionViewModel.cs
@@ -17,6 +17,10 @@ namespace Mergen.Admin.Api.API.Questions
         public string Answer2 { get; set; }
         public string Answer3 { get; set; }
         public string Answer4 { get; set; }
+        public long Answer1ChooseHistory { get; set; }
+        public long Answer2ChooseHistory { get; set; }
+        public long Answer3ChooseHistory { get; set; }
+        public long Answer4ChooseHistory { get; set; }
         public int CorrectAnswerNumber { get; set; }
         public IEnumerable<string> CategoryIds { get; set; }
 
@@ -32,6 +36,10 @@ namespace Mergen.Admin.Api.API.Questions
                 Answer2 = question.Answer2,
                 Answer3 = question.Answer3,
                 Answer4 = question.Answer4,
+                Answer1ChooseHistory = question.Answer1ChooseHistory,
+                Answer2ChooseHistory = question.Answer2ChooseHistory,
+                Answer3ChooseHistory = question.Answer3ChooseHistory,
+                Answer4ChooseHistory = question.Answer4ChooseHistory,
                 CorrectAnswerNumber = question.CorrectAnswerNumber,
                 CategoryIds = question.CategoryIdsCache.Split(_categoryIdsCacheSeparator, StringSplitOptions.RemoveEmptyEntries)
             };
