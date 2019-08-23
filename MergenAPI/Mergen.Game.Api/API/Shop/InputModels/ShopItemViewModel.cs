0a75ba1ad39c5081cf4d1c1cbc109bc8b98baf21
Ashkan Nourzadeh <a.nourzadeh@mabnadp.com>
2019-08-23 20:52:47 +0430

    fix ShopItemViewModel

--- MergenAPI/Mergen.Game.Api/API/Shop/InputModels/ShopItemViewModel.cs
+++ MergenAPI/Mergen.Game.Api/API/Shop/InputModels/ShopItemViewModel.cs
@@ -7,14 +7,16 @@ namespace Mergen.Game.Api.API.Shop.InputModels
     public class ShopItemViewModel : EntityViewModel
     {
         public string Title { get; set; }
-        public string TypeId { get; set; }
-        public string AvatarCategoryId { get; set; }
+        public int TypeId { get; set; }
+        public int AvatarCategoryId { get; set; }
+        public int? AvatarTypeId { get; set; }
+        public bool? DefaultAvatar { get; set; }
         public string PriceTypeId { get; set; }
         public decimal Price { get; set; }
         public string ImageFileId { get; set; }
         public int? UnlockLevel { get; set; }
         public int? UnlockSky { get; set; }
-        public string StatusId { get; set; }
+        public int StatusId { get; set; }
         public string Description { get; set; }
         public int? Quantity { get; set; }
 
