f3e92d82615e7990ea0e2a24a0dcb44d879b80bd
Ashkan Nourzadeh <ans.ashkan@gmail.com>
2019-08-25 19:25:46 +0430

    add files/{fileId}/data api to GameAPI

---
+++ MergenAPI/Mergen.Game.Api/API/UploadedFiles/UploadedFileController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Mergen.Core.Managers;
+using Mergen.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mergen.Game.Api.API.UploadedFiles
+{
+    [ApiController]
+    public class UploadedFileController : ApiControllerBase
+    {
+        private readonly IFileService _fileService;
+        private readonly FileManager _fileManager;
+
+        public UploadedFileController(IFileService fileService, FileManager fileManager)
+        {
+            _fileService = fileService;
+            _fileManager = fileManager;
+        }
+
+        [HttpGet]
+        [Route("files/{fileId}/data")]
+        public async Task<ActionResult> GetData([FromRoute] string fileId, CancellationToken cancellationToken)
+        {
+            var file = await _fileManager.GetFileByFileIdAsync(fileId, cancellationToken);
+            if (file == null)
+                return NotFound();
+
+            return File(_fileService.GetFile(file.FileId), file.MimeType, true);
+        }
+    }
+}
